9462fdc0b25dfd528576374831e9157e14f1006c
Ken Mbira <devmbira>
2023-06-23 08:56:01 +0300

    add landing hero section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { LandingComponent } from './home/landing/landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,

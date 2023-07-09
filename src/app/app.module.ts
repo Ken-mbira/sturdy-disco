e8c2c9b0cb5e6a694816493b393705714ea63483
Ken Mbira <devmbira>
2023-07-09 22:49:24 +0300

    modify tech list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { LandingComponent } from './home/landing/landing.component';
+import { TechListComponent } from './home/tech-list/tech-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    LandingComponent
+    LandingComponent,
+    TechListComponent
   ],
   imports: [
     BrowserModule,

8254e3f97c04588b3e94f7ff127044a1973193eb
Ken Mbira <devmbira>
2023-07-12 05:41:55 +0300

    implement insights section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { LandingComponent } from './home/landing/landing.component';
 import { TechListComponent } from './home/tech-list/tech-list.component';
+import { InsightsComponent } from './home/insights/insights.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { TechListComponent } from './home/tech-list/tech-list.component';
     NavbarComponent,
     HomeComponent,
     LandingComponent,
-    TechListComponent
+    TechListComponent,
+    InsightsComponent
   ],
   imports: [
     BrowserModule,
